d0ccc9b1c3cc8d8c0a4f57ec5cbf130ea723e430
DHBaek <baek@comcom.ai>
2023-08-21 13:27:58 +0900

    feat: update available models

--- src/event_handlers.py
+++ src/event_handlers.py
@@ -7,7 +7,7 @@ from firebase_admin import credentials, db
 from loguru import logger
 
 from config import celery_settings, firebase_settings
-
+from enums import ModelEnum
 
 def _setup_firebase(app: FastAPI) -> None:
     cred = credentials.Certificate(firebase_settings.firebase_cred_path)
@@ -19,14 +19,11 @@ def _setup_firebase(app: FastAPI) -> None:
 
 def _setup_celery(app: FastAPI) -> None:
     logger.info("Setup Celery")
-    ref = db.reference(f"{firebase_settings.firebase_app_name}/models")
-    data = ref.get()
-    if data is None:
-        exit()
     app.state.celery = {}
-    for model_id in data.keys():
+    for model in ModelEnum:
+        model_id = model.value
         app.state.celery[model_id] = Celery(broker=f"{celery_settings.broker_base_uri}/{model_id}")
-
+        
 
 def start_app_handler(app: FastAPI) -> Callable:
     def startup() -> None:
